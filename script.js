9128c1cd7d0bb5c8b0c677bf574d650cd9dfd593
akunya kamu <torimuru28@gmail.com>
2025-02-02 15:24:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Menambahkan event listener pada tombol "Buka Surat"
-document.getElementById('openBtn').addEventListener('click', function() 
-                                                    {  
+document.getElementById('openBtn').addEventListener('click', function(){                                            
     // Menampilkan surat dengan animasi fade-in
     var letter = document.getElementById('letter');
     letter.classList.add('show');
