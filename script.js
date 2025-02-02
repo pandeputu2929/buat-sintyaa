4bdae4cbcd535fa2f5c8d11301efe715ac1c196c
pande <torimuru28@gmail.com>
2025-02-02 15:28:42 +0800

    mastes

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 // Menambahkan event listener pada tombol "Buka Surat"
 document.getElementById('openBtn').addEventListener('click', function() {
-    // Menampilkan popup sistem
-    alert("Sistem: Surat sedang dibuka...");
-    
     // Menampilkan surat dengan animasi fade-in
     var letter = document.getElementById('letter');
     letter.classList.add('show');
